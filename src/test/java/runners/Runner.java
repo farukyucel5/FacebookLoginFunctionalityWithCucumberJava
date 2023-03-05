c640644f3a468e4e286c0ddb2fa199fda36135d0
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-03-05 13:58:19 +0300

    allure reporting

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,11 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-     plugin={"html:target/cucumber-reports-smoke.html",
-              "json:target/json-reports/cucumber.json",
-             "junit:target/xml-report/cucumber.xml",
-            },
-        //plugin ={"pretty:target/allure-report/io.qameta.allure.cucumberjvm7.AllureCucumber7Jvm"},
+     //plugin={"html:target/cucumber-reports-smoke.html",
+     //        "json:target/json-reports/cucumber.json",
+     //       "junit:target/xml-report/cucumber.xml",
+     //      },
+
+        plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         features = "src/test/resources/features" ,
         glue = "stepdefinitions" ,
         tags= "@smoke",
